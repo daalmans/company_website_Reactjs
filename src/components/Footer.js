a597284783b0ac67171960202d27bc9d748e7796
Maciej Kraczla <maciekkra@gmail.com>
2020-05-17 14:47:07 +0200

    css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import ContactFooter from './ContactFooter'
 
 export const Footer = () => {
     return (
-        
+
         <footer className="footer">
             <div className="wrapper wrapper--footer">
 
@@ -15,8 +15,17 @@ export const Footer = () => {
                 </div>
                 <div className="footer__blog-posts">
                     {/* BLOG POSTS COMING SOON */}
-                    </div>
+                </div>
 
+
+
+            </div>
+            <div className="footer__copy">Strona jest własnością 
+            <span>
+            <a href="https://smgkrc.com/">
+                     SMG/KRC Poland Human Resources
+                </a>
+                </span>
             </div>
         </footer>
     )
