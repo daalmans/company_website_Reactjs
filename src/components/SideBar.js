3763f332f8c5fb613a3c111f724e38cb6f6ec83c
Maciej Kraczla <maciekkra@gmail.com>
2020-05-17 14:07:45 +0200

    css

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,7 @@ import { Slide } from 'react-reveal';
 const SideBar = ({showSideBar}) => {
     return (        
         <Slide duration={500} right in={showSideBar}>
-            <div style={{display: `${showSideBar ? 'block' : 'none'}`}} className="sidebar clearfix">
+            <div style={{display: `${showSideBar ? 'block' : 'none'}`}} className="sidebar">
                 <Nav context="sidebar"/>
             </div>               
         </Slide>        
