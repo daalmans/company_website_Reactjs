a27841703e5dba16c547a290601506cbe7ccdc3d
Maciej Kraczla <maciekkra@gmail.com>
2020-05-17 13:32:31 +0200

    adjusted css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,37 +6,6 @@ import { Fade } from 'react-reveal';
 import SideBar from './SideBar';
 
 
-// class Header extends Component {
-
-//     state = {
-//         showSideBar: false
-//     }
-
-//     showSideBar = () => {
-//         const { showSideBar } = this.state;
-//         this.setState({ showSideBar: !showSideBar })
-//     }
-
-//     render() {
-//         const { showSideBar } = this.state;
-//         return (
-//             <Fade top delay={50}>
-//                 <header className="header">
-//                     <div className="wrapper wrapper--header">
-//                         <Link className="header__logo-link" to="/">
-//                             <div className="logo"></div>
-//                         </Link>
-//                         <Nav context="header" />
-//                         <div className="header__menu" onClick={this.showSideBar}></div>
-//                         <SideBar showSideBar={showSideBar} />
-//                     </div>
-//                 </header>
-//             </Fade>
-//         );
-//     }
-// }
-
-// export default Header;
 
 const Header = () => {
 
